db7bf5839668585ba9d37f1f6ecdd3f764d8502e
Toby Ng <tobyng01@gmail.com>
2017-12-08 23:21:41 +0800

    removed untracked files

--- ref/ncc_download.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-ncc_google_sheets_url = "https://docs.google.com/spreadsheets/d/1zpMpR7deHB4fQByw45g7mVzTo0iE17lSmDeu66XpXyQ/export?format=csv"
-response = urllib.request.urlopen(ncc_google_sheets_url)
-data = response.read()
-text = data.decode('utf-8')
-print(text)
